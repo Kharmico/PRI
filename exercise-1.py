bde82746d7598fd10043762d9be3958613241d93
pauloanjos <paulo.15.gk@gmail.com>
2017-10-14 15:08:40 +0100

    fix test file had \n where it sould not

--- exercise-1.py
+++ exercise-1.py
@@ -1,9 +1,15 @@
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+regexExpression = "[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[a-zA-Z]+"
 
+def stringToWords(text):
+	return re.findall(regexExpression, text)
 
 def readfile(filename):
 	f2=open(filename,"r")
-	test=f2.read()
-	print(test)
+	text=f2.read()
+	tempText=stringToWords(text)
+	print(tempText)
 
 readfile("teste.txt")
 
