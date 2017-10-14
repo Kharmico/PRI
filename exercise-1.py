b57ec51f7c1db1905dac9146b5a109afd8b98977
pauloanjos <paulo.15.gk@gmail.com>
2017-10-14 16:24:48 +0100

    get terms in the hole document, slipt text into dictionary f sentences each sentence is and array of terms

--- exercise-1.py
+++ exercise-1.py
@@ -1,15 +1,45 @@
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
-regexExpression = "[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[a-zA-Z]+"
+regexWords = "[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[a-zA-Z]+"
+regexSentences="(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
+terms=[]
+sentences =dict()
+def stringToTerms(text):
+	return re.findall(regexWords, text)
+def stringToDictOfSentences(text):
+	aux=[]
+	aux=re.split(regexSentences, text)
+	global sentences
+	i=0
+	for s in aux:
+		print(i)
+		sentences[i]=stringToTerms(s)
+		i+=1
 
-def stringToWords(text):
-	return re.findall(regexExpression, text)
+
+
+def getTermFromText(text):
+	global terms
+	auxTerms=stringToTerms(text)
+	for t in auxTerms:
+		if t not in terms:
+			terms+=[t.lower()]
+	
+
+
 
 def readfile(filename):
+	global terms,sentences
 	f2=open(filename,"r")
 	text=f2.read()
-	tempText=stringToWords(text)
-	print(tempText)
+	getTermFromText(text)
+	print("termos:")
+	print(terms)
+	stringToDictOfSentences(text)
+	print("frazes")
+	print(sentences)
+	#ntempText=stringToSentences(text)
+
 
 readfile("teste.txt")
 
