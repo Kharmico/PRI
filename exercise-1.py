fe7d604cc3ca0aed657243daf8f3c08183fceca9
pauloanjos <paulo.15.gk@gmail.com>
2017-10-17 12:31:07 +0100

    inverted list and set of terms

--- exercise-1.py
+++ exercise-1.py
@@ -1,45 +1,66 @@
 import re
+import math
 from sklearn.feature_extraction.text import TfidfVectorizer
-regexWords = "[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[a-zA-Z]+"
-regexSentences="(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
-terms=[]
+from collections import Counter
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import RegexpTokenizer
+terms=dict()
 sentences =dict()
+invertedList = dict()
+
+class TermClass:
+	term=""
+	isf=None
+	sf=None
+	maxTf=None
+	minTf=None
+	def __eq__(self, other):
+		if isinstance(other, SimilarityPair):
+			return ((self.term == other.term))
+		else:
+			return False
+	def __str__(self):
+		return self.term
+
 def stringToTerms(text):
-	return re.findall(regexWords, text)
+	tokenizer = RegexpTokenizer(r'\w+')
+	return tokenizer.tokenize(text) # todas as palavras do texto 
+
 def stringToDictOfSentences(text):
-	aux=[]
-	aux=re.split(regexSentences, text)
+	global terms,sentences,invertedList
+	aux_sentences = sent_tokenize(text)
+	i=1
 	global sentences
-	i=0
-	for s in aux:
-		print(i)
-		sentences[i]=stringToTerms(s)
+	for line in aux_sentences:
+		aux_terms=stringToTerms(line)
+		sentences[i] = aux_terms
+		for t in aux_terms:
+			obj= TermClass()
+			obj.term=t
+			if t not in terms:
+				terms[t]=obj
+				invertedList[t]=dict()
 		i+=1
-
-
-
-def getTermFromText(text):
-	global terms
-	auxTerms=stringToTerms(text)
-	for t in auxTerms:
-		if t not in terms:
-			terms+=[t.lower()]
-	
-
-
+		setNumOcorrencies()
+		
+		
+def setNumOcorrencies():
+	for t in terms:
+			for s in sentences:
+				fq=0
+				fq=sentences[s].count(t)
+				if fq >0 :
+					if s not in invertedList[t]:
+						invertedList[t][s]=fq
 
 def readfile(filename):
 	global terms,sentences
 	f2=open(filename,"r")
-	text=f2.read()
-	getTermFromText(text)
-	print("termos:")
-	print(terms)
+	text=f2.read().lower()
 	stringToDictOfSentences(text)
-	print("frazes")
-	print(sentences)
-	#ntempText=stringToSentences(text)
+	#print(sentences)
+	for k in invertedList:
+		print(str(k)+": "+str(invertedList[k]))
 
-
-readfile("teste.txt")
+readfile("smalltest.txt")
 
