9167147b26eb9216513c982e0a41105953404f06
pauloanjos <paulo.15.gk@gmail.com>
2017-10-28 20:02:01 +0100

    ex3 with bi-grams, it takes forever

--- exercise-3.py
+++ exercise-3.py
@@ -5,6 +5,7 @@ import math
 import os
 import operator
 import nltk 
+nltk.download('punkt')
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
@@ -16,6 +17,7 @@ from nltk.corpus import stopwords
 from string import punctuation
 from nltk.chunk import RegexpParser
 from nltk import ngrams
+from nltk.corpus import floresta
 
 
  # #1. we obtain the unigrams
@@ -44,7 +46,6 @@ PATH_AUTO_IDEAL_EXTRACTIVES='./AutoIdealExtractives/'
 RESUME_LEN = 5
 
 sentInExtResumes = 0
-terms = dict()
 invertedList = dict()#term-doc-sentence
 invertedListDoc=dict()#doc-term-sentence
 docs = [f for f in os.listdir(PATH_SOURCE_TEXT)]
@@ -55,6 +56,8 @@ scores=dict() #key=doc-sentence - respective score
 num_frases=0
 num_frases_termo=dict() # key= termo, 
                         #value = numero de fazes na colecao inteira em que o termo aparece
+sent_tokenizer=nltk.data.load('tokenizers/punkt/portuguese.pickle')
+stopwords = set(nltk.corpus.stopwords.words('portuguese'))
 
 class TermClass:
     term = ""
@@ -72,55 +75,71 @@ class TermClass:
     def __str__(self):  # equivalente a to string
         return self.term
 
-
 def stringToTerms(text):
-    tokenizer = RegexpTokenizer(r'\w+')
-    return tokenizer.tokenize(text) # todas as palavras do texto 
+	global stopwords
+	text=text.lower()
+	tokenizer = RegexpTokenizer(r'\w+')
+	unigrams= tokenizer.tokenize(text) # todas as palavras do texto 
+	unigrams = [word for word in set(unigrams) if word not in stopwords]
+	#2. we get the bigrams
+	bigrams = ngrams(unigrams, 2)
+	#3. we join the bigrams in a list like so (word word)
+	text_bigrams = [' '.join(grams) for grams in bigrams]
+	#print("text_bigrams")
+	#i=0
+	#for gram in text_bigrams:
+	#	print(gram)#4. we join the unigram and bigram list to get all the candidates
+	#	i+=1
+	#print("n text_bigrams "+str(i))
+	candidates = unigrams + text_bigrams
+	
+	return candidates# todas as palavras do texto 
 
 def DocToSentences(text):
-    frases_tokenize= tokenize.sent_tokenize(text, language='english')
-    return frases_tokenize
+	global sent_tokenizer
+	frases_tokenize = sent_tokenizer.tokenize(text)
+	return frases_tokenize
 
 def setInvertedList(docs):
-    global terms,invertedList,invertedListDoc,num_frases,OriginalDocs
+    global invertedList,invertedListDoc,num_frases,OriginalDocs
     #para cada doc
+    #print("inverted list")
     num_frases=0
     for doc in docs:
         f2 = open(PATH_SOURCE_TEXT+doc, "r")
         #f2 = open(doc, "r")
         text = f2.read()
+        f2.close()
         OriginalDocs[doc]=text
-        text = text.lower()
+        #text = text.lower()
         sentences = DocToSentences(text)
         invertedListDoc[doc]=dict()
         sentence_counter=1
         for sentence in sentences:
             num_frases+=1
             aux_terms=stringToTerms(sentence)
-            for t in aux_terms:
-                obj= TermClass()
-                obj.term=t
+            for t in set(aux_terms):
                 if t not in invertedListDoc[doc]:
                     invertedListDoc[doc][t]=dict()
                 if sentence_counter not in invertedListDoc[doc][t]:
                     invertedListDoc[doc][t][sentence_counter]=0
                 invertedListDoc[doc][t][sentence_counter]+=1
-                if t not in terms:
-                    terms[t]=obj
+                if t not in invertedList:
                     invertedList[t]=dict()
                 if doc not in invertedList[t]:
                     invertedList[t][doc]=dict()
+                invertedList[t][doc][sentence_counter]=aux_terms.count(t)
             sentence_counter+=1
-    #print("inverted list DOC:----------------------------------")
-    #print(str(invertedListDoc))
-    populateInvertedList(docs)
+    #print("inverted list:----------------------------------")
+    #print(str(invertedList.keys()))
+    #populateInvertedList(docs)
 
 def populateInvertedList(docs):
     global invertedList,OriginalDocs
     for doc in docs:
         #f2 = open(PATH_SOURCE_TEXT+doc, "r")
         #f2 = open(doc, "r")
-        text = OriginalDocs[doc].lower()
+        text = OriginalDocs[doc]
         sentences = DocToSentences(text)
         sentence_counter=1      
         for sentence in sentences:
@@ -184,19 +203,16 @@ def sqrtSomeSquares(doc,sentence,tfIdf):
 
 
 def calcpesoTermoDoc(doc,tfIdf):
-    #TODO:
     pesosDoc= dict()
     maxTf=getFqMaxDoc(doc)
     for sentence in tfIdf[doc]:
         for term in tfIdf[doc][sentence]:
-            if term not in pesosDoc.keys():
+            if term not in pesosDoc:
                 pesosDoc[term]=((getFqTermDoc(term,doc)/maxTf) * idf(term,doc))
     return pesosDoc
 
 def sumMultiPesos(doc,sentence,pesosDoc,tfIdf):
-    #TODO:
     value=0
-    maxTf=getFqMaxDoc(doc)# Tf maximo dos termos no documento
     for term in tfIdf[doc][sentence]:
         value+=(tfIdf[doc][sentence][term] * pesosDoc[term])
         #print("sumMultiPesos "+ str(sentence)+" "+term+" "+str(value))
@@ -210,11 +226,9 @@ def sqrtSomeSquaresDoc(pesosDoc):
     return math.sqrt(value)
 
 def calculateScoreOfsentences(doc,tfIdf):
-    #TODO:
     global invertedListDoc
     pesosDoc=dict()
-    pesosDoc=calcpesoTermoDoc(doc,tfIdf)
-    
+    pesosDoc=calcpesoTermoDoc(doc,tfIdf)  
     sentences_scores=dict()
     for sentence in tfIdf[doc]:
         sqrt_some_squares=sqrtSomeSquares(doc,sentence,tfIdf)
@@ -286,13 +300,14 @@ def saveResumes():
     extracted = dict()
 
     for doc in resumes:
-        file = open(PATH_AUTO_IDEAL_EXTRACTIVES + doc).read()
-        sentences = DocToSentences(file)
-        docSeparate = doc.split("-")
-        docToSave = docSeparate[1] + "-" + docSeparate[2]
-        #print("Doc To Save: " + docToSave)
-        extracted[docToSave] = sentences
-        sentInExtResumes += len(sentences)
+    	f2=open(PATH_AUTO_IDEAL_EXTRACTIVES + doc)
+    	file = f2.read()
+    	f2.close()
+    	sentences = DocToSentences(file)
+    	docSeparate = doc.split("-")
+    	docToSave = docSeparate[1] + "-" + docSeparate[2]#print("Doc To Save: " + docToSave)
+    	extracted[docToSave] = sentences
+    	sentInExtResumes += len(sentences)
     return extracted
 
 def calc_avg_doc(our_ResumeSents, ideal_ResumeSents):
@@ -313,14 +328,6 @@ def calc_avg_doc(our_ResumeSents, ideal_ResumeSents):
         iteration += 1
     avg_precision=avg_precision/len(ideal_ResumeSents)
     return avg_precision
-
-def calc_MAP(our_Resume, ideal_Resume):
-    mean_avg_precision = 0
-    for doc in docs:
-        mean_avg_precision += calc_avg_doc(our_Resume[doc], ideal_Resume[doc])
-
-    mean_avg_precision = mean_avg_precision/len(our_Resume.keys())
-    return mean_avg_precision
 
 def main():
     global docs
@@ -346,10 +353,10 @@ def main():
     mean_avg_precision = mean_avg_precision/num_docs
 
     print("--- Metrics for 1st Exercise Approach")
-    print("Precision: " + str(prec1))
-    print("Recall : " + str(rec1))
+    print("Precision: " + str(prec))
+    print("Recall : " + str(rec))
     print("F1 : " + str(_f11))
-    print("MAP : " + str(mean_avg_precision1))
+    print("MAP : " + str(mean_avg_precision))
 
 main()
 
