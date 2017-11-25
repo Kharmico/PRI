558ffb89ea78584495be18b33778ab822c462782
Paulo Anjos <pauloanjos@users.noreply.github.com>
2017-11-25 20:57:25 +0000

    Delete exercise-test.py

--- exercise-test.py
+++
@@ -1,9 +0,0 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-filedata = open("teste.tx").read()
-
-sentences = sent_tokenize(filedata)
-words = []
-
-for lines in sentences:
-	words.append(word_tokenize(lines))
